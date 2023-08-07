1e109087c1145b48a70588936a8fe1f7bda109c7
CKATYAN <ct.rainfox2017@gmail.com>
2023-08-07 19:01:04 +0700

    add js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const homeUrl = "https://ckatyan.github.io"
+$(document).ready(()=>{
+    let currentUrl = window.location.href;
+    if (currentUrl == "https://ckatyan.github.io/about.html") {
+        window.location.replace(homeUrl);
+        document.getElementById("pagename").textContent="About project";
+    }
+})
