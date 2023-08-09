f0ddc5e45b0c12994248b2dadd3d92e02941a9f1
CKATYAN <ct.rainfox2017@gmail.com>
2023-08-09 17:53:49 +0700

    Linking

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const homeUrl = "https://ckatyan.github.io"
 switch (window.location.href) {
-    case "https://ckatyan.github.io#about.html":
+    case "https://ckatyan.github.io/#about.html":
         document.getElementById("pagename").textContent="About project";
         break;    
-    case "https://ckatyan.github.io#contact.html":
+    case "https://ckatyan.github.io/#contact.html":
         document.getElementById("pagename").textContent="Contact";
         break;   
 }
