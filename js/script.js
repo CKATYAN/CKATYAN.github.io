b5d177fb1d44712eb454931b74bdb36466046604
CKATYAN <ct.rainfox2017@gmail.com>
2023-08-10 17:14:25 +0700

    Css is done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-const homeUrl = "https://ckatyan.github.io"
-switch (window.location.href) {
-    case "https://ckatyan.github.io/#about":
-        document.getElementById("pagename").textContent="About project";
-        break;    
-    case "https://ckatyan.github.io/#contact":
-        document.getElementById("pagename").textContent="Contact";
-        break;   
+const homeUrl = window.location.origin
+console.log(homeUrl)
+
+var writeHeaderBottom = (event) => {
+    switch (window.location.href) {
+        case (homeUrl + "/#about"):
+            document.getElementById("pagename").textContent="About project"
+            break
+        case (homeUrl + "/#contact"):
+            document.getElementById("pagename").textContent="Contact"
+            break
+    }
 }
+window.addEventListener("hashchange", writeHeaderBottom, false)
+window.addEventListener("load", writeHeaderBottom, false)
+
+
